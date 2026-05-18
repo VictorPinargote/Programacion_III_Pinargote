2bffc7460d1ce3d8a809754bb570e8c8509cb190
VictorPinargote <joelpinargote76@gmail.com>
2026-05-18 09:03:18 -0500

    Clase 18-05-2026

--- Modulo 3/blog-backend/src/app.controller.ts
+++ Modulo 3/blog-backend/src/app.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'; // Agregamos Put aquí
 import { AppService } from './app.service';
 import { ProductDto } from './product.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  
   @Get("/health")
   getHeath(): any {
     return this.appService.getHeath();
@@ -20,8 +21,25 @@ export class AppController {
     return this.appService.findAll();
   }
 
-  @Get("/health")
+  @Get("/products/:id") // Corregido: Agregamos /:id para que no choque con findAll
   findById(@Param('id') id: string): ProductDto {
     return this.appService.findById(id);
+  }
+
+  @Put("/products/:id")
+  update(
+    @Param("id") id: string,
+    @Body() updatedProduct: ProductDto): any {
+    return this.appService.update(id, updatedProduct); // Corregido: updatedProduct con "d"
+  }
+  
+  @Delete("/products/:id")
+  deleteById(@Param("id") id: string): ProductDto {
+    return this.appService.deleteById(id);
+  }
+
+  @Post("/area-triangulo")
+  areaTriangulo(@Body() data: any): any {
+    return this.appService.areaTriangulo(data);
   }
 }
--- Modulo 3/blog-backend/src/app.service.ts
+++ Modulo 3/blog-backend/src/app.service.ts
@@ -3,33 +3,29 @@ import { ProductDto } from './product.dto';
 
 @Injectable()
 export class AppService {
-  private products: ProductDto[] ={
+  private products: ProductDto[] = [{
     id: 2,
     name: 'Laptop DELL',
     price: 1000,
     stock: 10
-  }
+  }];
+
   getHeath(): any {
     return {
-      "status": "Online",
-      "service": "blog service api",
-      "version": "0.0.1",
-      "date": new Date()
+      status: 'Online',
+      service: 'blog service api',
+      version: '0.0.1',
+      date: new Date()
     };
   }
 
   createProduct(product: ProductDto): ProductDto {
-    const newProduct: ProductDto={
-      id: Math.floor(Math.random()*1000)+1,
-      ...product
-    }
-    this.products.push(newProduct);
-    return {
-      "id": product.id,
-      "name": product.name,
-      "price": product.price,
-      "stock": product.stock
+    const newProduct: ProductDto = {
+      ...product, // Corregido: Primero esparcimos el producto...
+      id: Math.floor(Math.random() * 1000) + 1 // ...luego asignamos nuestro propio ID para que no se sobreescriba
     };
+    this.products.push(newProduct);
+    return newProduct;
   }
 
   findAll(): ProductDto[] {
@@ -37,7 +33,35 @@ export class AppService {
   }
 
   findById(id: string): ProductDto {
-    return this.products!
-        .find(product=>product.id===Number(id))!;
+    return this.products.find(product => product.id === Number(id))!;
+  }
+
+  update(id: string, updatedProductDto: ProductDto): any {
+    const product = this.products.find(product => product.id === Number(id));
+    if (!product) {
+      return;
+    }
+    Object.assign(product, updatedProductDto);
+    return product;
+  }
+
+  deleteById(id: string): any {
+    const index = this.products!
+        .findIndex(product => product.id === Number(id))!;
+    if (index === -1){
+      return;
+    }
+    const deleteProduct = this.products[index];
+    this.products.splice(index, 1);
+    return deleteProduct;
+  }
+
+  areaTriangulo(data: any): any {
+    const area = (data.base * data.altura)/2;
+    return {
+      "base": data.base,
+      "altura": data.altura,
+      "areatriangulo": area
+    };
   }
 }
